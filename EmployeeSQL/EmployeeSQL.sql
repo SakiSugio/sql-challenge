5b3ea97425ec11863d5978bdd6b505c973d9f444
Saki Sugio <sakisugio9@gmail.com>
2020-09-22 22:43:18 -0500

    no 2

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -5,3 +5,8 @@ FROM employees
 INNER JOIN salaries ON
 employees.emp_no=salaries.emp_no;
 
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
