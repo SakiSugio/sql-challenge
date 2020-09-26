e30fa200602bdb5f77e55c095470eb58d4159d06
Saki Sugio <sakisugio9@gmail.com>
2020-09-25 19:49:43 -0500

    no 3

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -10,3 +10,10 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
+--3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+INNER JOIN dept_manager ON
+departments.dept_no=dept_manager.dept_no
+INNER JOIN employees ON
+dept_manager.emp_no=employees.emp_no;
