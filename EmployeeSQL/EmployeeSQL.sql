c825d64d28b8ee964825003ae87b682060d7dffa
Saki Sugio <sakisugio9@gmail.com>
2020-09-25 20:38:48 -0500

    no 6

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -17,3 +17,26 @@ INNER JOIN dept_manager ON
 departments.dept_no=dept_manager.dept_no
 INNER JOIN employees ON
 dept_manager.emp_no=employees.emp_no;
+
+--4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM departments
+INNER JOIN dept_emp ON
+departments.dept_no=dept_emp.dept_no
+INNER JOIN employees ON
+dept_emp.emp_no=employees.emp_no;
+
+--5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT employees.first_name, employees.last_name, employees.sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT employees.last_name, employees.first_name, departments.dept_name
+FROM departments
+INNER JOIN dept_emp ON
+departments.dept_no=dept_emp.dept_no
+INNER JOIN employees ON
+dept_emp.emp_no=employees.emp_no
+WHERE departments.dept_name = 'Sales' ;
+
