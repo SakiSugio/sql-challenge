0346f122ec5db1ac6673c4508753667b45642eee
Saki Sugio <sakisugio9@gmail.com>
2020-09-25 21:45:52 -0500

    main part done

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -32,11 +32,26 @@ FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
-SELECT employees.last_name, employees.first_name, departments.dept_name
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM departments
 INNER JOIN dept_emp ON
 departments.dept_no=dept_emp.dept_no
 INNER JOIN employees ON
 dept_emp.emp_no=employees.emp_no
 WHERE departments.dept_name = 'Sales' ;
+
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM departments
+INNER JOIN dept_emp ON
+departments.dept_no=dept_emp.dept_no
+INNER JOIN employees ON
+dept_emp.emp_no=employees.emp_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development';
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT employees.last_name, COUNT(*)
+FROM employees
+GROUP BY employees.last_name
+ORDER BY "count" DESC;
 
